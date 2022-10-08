704103df7cae45da6eb2605abd57a0d5af5be358
Vinesh <vineshgobin@gmail.com>
2022-10-08 18:36:32 -0400

    Commented line

--- src/api/EmployeeService.js
+++ src/api/EmployeeService.js
@@ -1,46 +1,52 @@
 // import axios from 'axios'
-import API_URL from '../Constants'
+// import API_URL from '../Constants'
 
 export const getEmployees = async () => {
   try {
-    const response = await fetch('https://apex.oracle.com/pls/apex/hoangtuanmai/api/employees?limit=500')
+    const response = await fetch(
+      "https://apex.oracle.com/pls/apex/hoangtuanmai/api/employees?limit=500"
+    );
     // const response = await fetch(`${API_URL}/employees?limit=500`)
 
-    const data = await response.json()
-    return data.items
+    const data = await response.json();
+    return data.items;
   } catch (error) {
-    console.log({ error })
+    console.log({ error });
   }
-}
+};
 
-export const createEmployee = async ({
-  body
-}) => {
+export const createEmployee = async ({ body }) => {
   try {
-    const response = await fetch('https://apex.oracle.com/pls/apex/hoangtuanmai/api/employee', {
-      // const response = await fetch(`${API_URL}/employee`, {
-      method: 'POST',
-      body: JSON.stringify(body),
-    })
-    return response
+    const response = await fetch(
+      "https://apex.oracle.com/pls/apex/hoangtuanmai/api/employee",
+      {
+        // const response = await fetch(`${API_URL}/employee`, {
+        method: "POST",
+        body: JSON.stringify(body),
+      }
+    );
+    return response;
   } catch (error) {
-    console.log({ error })
+    console.log({ error });
   }
-}
+};
 
 export const deleteEmployee = async (id) => {
   //console.log('executed service')
   // return axios.delete(`${API_URL}/employee/${id}`);
   try {
-    const response = await fetch(`https://apex.oracle.com/pls/apex/hoangtuanmai/api/employee/${id}`, {
-      // const response = await fetch(`${API_URL}/employee/${id}`, {
-      method: 'DELETE',
-    })
-    return response
+    const response = await fetch(
+      `https://apex.oracle.com/pls/apex/hoangtuanmai/api/employee/${id}`,
+      {
+        // const response = await fetch(`${API_URL}/employee/${id}`, {
+        method: "DELETE",
+      }
+    );
+    return response;
   } catch (error) {
-    console.log({ error })
+    console.log({ error });
   }
-}
+};
 
 // export const updateEmployee = (id, firstname, middlename, lastname, soc, hiredate, salary, commissionpct, departmentcode, jodcode, managerid) => {
 //   //console.log('executed service')
